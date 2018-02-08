44e04dfce974b1f970a5af3d9ecd9cccbf9b04e2
workPC <448147876@qq.com>
2018-02-08 11:23:15 +0800

    添加sql建表语句 修改商品列表请求测试

--- taotaoManager/taotaoManagerWeb/src/main/java/com/taotao/controller/ItemController.java
+++ taotaoManager/taotaoManagerWeb/src/main/java/com/taotao/controller/ItemController.java
@@ -12,11 +12,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
@@ -27,28 +23,59 @@ import java.sql.SQLException;
 import java.util.List;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
 
 	@Autowired
 	private ItemService itemService;
 	@Autowired
 	private TbItemMapper itemMapper;
-	
-	@RequestMapping("/item/{itemId}")
+
+
+	/**
+	 * 进行post请求，接受参数为pojo
+	 * @param tbItem
+	 * @return
+	 */
+	@RequestMapping(value = "/itemId01",method = RequestMethod.POST)
 	@ResponseBody
-	public TbItem getItemById(@PathVariable Long itemId) {
-		TbItem tbItem = itemService.getItemById(itemId);
-//		TbItemExample example = new TbItemExample();
-//		TbItemExample.Criteria criteria = example.createCriteria();
-//		criteria.andIdEqualTo(itemId);
-//		List<TbItem> list = itemMapper.selectByExample(example);
-//		for(TbItem t:list){
-//			return t;
-//		}
-
-
-		return tbItem;
+	public TbItem getItemById01(TbItem tbItem) {
+		if(tbItem != null){
+			TbItem tbItemQry = itemService.getItemById(tbItem.getId());
+			return tbItemQry;
+		}else{
+			return tbItem;
+		}
 	}
+
+	/**
+	 * 进行post请求，使用get方式，接收参数为商品id
+	 * @param tbItemId
+	 * @return
+	 */
+	@RequestMapping(value = "/itemId02",method = RequestMethod.GET)
+	@ResponseBody
+	public TbItem getItemById02(@RequestParam("itemId") Long tbItemId) {
+			TbItem tbItemQry = itemService.getItemById(tbItemId);
+			return tbItemQry;
+	}
+
+	/**
+	 * 进行post请求，使用get方式，接收参数为商品id
+	 * @param tbItemId
+	 * @return
+	 */
+	@RequestMapping(value = "/itemId03",method = RequestMethod.POST)
+	@ResponseBody
+	public List<TbItem> getItemById03(Long tbItemId) {
+		TbItemExample example = new TbItemExample();
+		TbItemExample.Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(tbItemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+
+		return list;
+	}
+
 
 	@RequestMapping("/list")
 	public String getItem(){
